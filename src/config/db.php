d88732605e5edab1eb2a090ed8aecf723db9ae11
deepanshi43 <deepanshi040305@gmail.com>
2026-05-17 17:20:26 +0530

    updated railway DB again updated

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,6 @@
 <?php
 // $conn = new mysqli("db", "root", "root", "club_db");
-$conn = new mysqli("mysql.railway.internal", "root", "ExgwGvYGVTFirgppBvKOEffrLwtiPbnq", "railway");
+$conn = new mysqli("mysql.railway.internal", "root", "root", "club_db",3306);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
