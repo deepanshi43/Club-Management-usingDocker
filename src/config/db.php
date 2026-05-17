a05f72649c4d33da15ac168f009909e93066553a
deepanshi43 <deepanshi040305@gmail.com>
2026-05-17 19:51:44 +0530

    7th time

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,6 @@
 <?php
 // $conn = new mysqli("db", "root", "root", "club_db");
-$conn = new mysqli("autorack.proxy.rlwy.net", "root", "ExgwGvYGVTFirgppBvKOEffrLwtiPbnq", "club_db",31674);
+$conn = new mysqli("autorack.proxy.rlwy.net", "root", "ExgwGvYGVTFirgppBvKOEffrLwtiPbnq", "railway",31674);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
